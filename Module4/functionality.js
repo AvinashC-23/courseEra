5e04b85958ca48fb028ab5e40d9b7c48d0f6d977
AvinashC-23 <54666554+AvinashC-23@users.noreply.github.com>
2022-11-20 20:36:53 +0530

    Create functionality.js

---
+++ Module4/functionality.js
@@ -0,0 +1,10 @@
+
+var names=["Avinash", "Rachita","John","Riana","Ananya","Janu","josh","Dude","Joy","Amigo"];
+
+for(var i=0;i<names.length;i++){
+    if(names[i].charAt(0).toLowerCase()==='j'){
+        console.log("GoodBye "+names[i])
+    }
+    else
+        console.log("Hello "+names[i])
+}
